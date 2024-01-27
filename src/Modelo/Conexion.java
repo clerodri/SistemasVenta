8d731abf2d7b8e1bfd5c6c151fd971b5720cb4d3
Ronaldo Rodriguez <clerodri@espol.edu.ec>
2024-01-27 00:32:49 -0500

    AGREGANDO CONEXION A JAMA

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.sql.Connection;
+
+
+
+/**
+ *
+ * @author ING. NM
+ */
+public class Conexion {
+    Connection con;
+    
+}
